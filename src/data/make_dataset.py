bbc070fe96eca4a4ce5525d4d0efcda691055843
philippe.matter68 <philippe.matter68@gmail.com>
2024-07-05 15:03:20 +0200

    adapted the logging info so that it gets printed in a more readable way.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -16,21 +16,21 @@ def main(input_filepath, output_filepath):
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data - yes so cool ! ')
+    logger.info('\nmaking final data set from raw data - yes so cool ! \n')
     # Data Preparation
     data = pd.read_csv(input_filepath)
     # Data Exploration
-    logger.info(f"Successfully read the csv file called {input_filepath}. \nHere are the first rows:\n\n {data.head()}")
-    logger.info(f"Dataframe shape : {data.shape}")
-    logger.info(f"Number of missing values: {data.isna().sum()}")
+    logger.info(f"\nSuccessfully read the csv file called {input_filepath}. \nHere are the first rows:\n\n {data.head()}\n")
+    logger.info(f"\nDataframe shape : {data.shape}\n")
+    logger.info(f"\nNumber of missing values:\n {data.isna().sum()}\n")
     # Data Cleaning
     data = data.drop(columns="total_bedrooms")
-    logger.info("Droped the total_bedrooms column.")
+    logger.info("\nDropped the total_bedrooms column.\n")
     # Train-Test Split
     data_train, data_test = train_test_split(data, test_size=0.33, random_state=0)
-    logger.info(f"Tarin-test split done:\n"
-                f"Train datasset has following shape: {data_train.shape},"
-                f"Test datasset has following shape: {data_test.shape}")
+    logger.info(f"\nTrain-test split done:"
+                f"\n--->Train datasset has following shape: {data_train.shape}"
+                f"\n--->Test datasset has following shape: {data_test.shape}\n")
     # Select X and y values (predictor and outcome)
     X_train = data_train.drop(columns="median_house_value")
     y_train = data_train["median_house_value"]
@@ -38,12 +38,12 @@ def main(input_filepath, output_filepath):
     y_test = data_test["median_house_value"]
     X_train.shape, X_test.shape
     write_train_test_data(output_filepath, X_train, X_test, y_train, y_test)
-    logger.info("Writing train-test data done.")
+    logger.info("\nWriting train-test data SUCCESSFULLY done. Yeahai !\n")
 
 def write_train_test_data(output_filepath, X_train, X_test, y_train, y_test):
     logger=logging.getLogger(__name__)
     output_paths = generate_train_test_addresses(output_filepath)
-    logger.info(f"Writing train-test data into\n{output_paths}.")
+    logger.info(f"\nWriting train-test data into:\n{output_paths}.\n")
     X_train.to_csv(output_paths['train_feature_path'], index=False)
     X_test.to_csv(output_paths['test_feature_path'], index=False)
     y_train.to_csv(output_paths['train_target_path'], index=False)
